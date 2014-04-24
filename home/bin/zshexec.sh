ffff10645c8039c322dafeb562fd889a71229035
kakakaya <kakakaya+git@gmail.com>
2014-04-24 18:38:47 +0900

    zshexec.sh:added branch for non-font env

--- home/bin/zshexec.sh
+++ home/bin/zshexec.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/zsh
 # -*- coding:utf-8 -*-
 PROCCOUNT=`expr \`ps -Afl | wc -l\` - 5`
-if [[ -x /usr/bin/figlet ]]; then figlet -f colossal $host;fi
+if [[ -x /usr/bin/figlet ]]; then
+    if [[ -e /usr/share/figlet/colossal.flf ]]; then
+	figlet -f colossal $host 2>/dev/null
+    else 
+	figlet $host 2>/dev/null
+    fi
+fi
 w
 
