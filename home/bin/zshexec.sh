ca1dd0fdd4c22cc3ff157665e4276bdc962dd648
kakakaya <kakakaya+git@gmail.com>
2014-04-24 13:48:36 +0900

    zshexec:changed Azurite to Azurite

--- home/bin/zshexec.sh
+++ home/bin/zshexec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 # -*- coding:utf-8 -*-
 PROCCOUNT=`expr \`ps -Afl | wc -l\` - 5`
-if [[ -x /usr/bin/figlet ]]; then figlet -f colossal $HOST;fi
+if [[ -x /usr/bin/figlet ]]; then figlet -f colossal $host;fi
 w
 
