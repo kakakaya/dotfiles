4c300993c9426213d57d3d5795d6d1009e29fc07
kakakaya <kakakaya+git@gmail.com>
2014-06-07 12:04:00 +0900

    init.el:tabbar.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -501,5 +501,36 @@
 (define-key ac-menu-map "\C-p" 'ac-previous)
 (setq ac-menu-height 10)
 
-
-
+;; tabbar
+(require 'tabbar)
+(tabbar-mode)
+(global-set-key "\M-]" 'tabbar-forward)  ; 次のタブ
+(global-set-key "\M-[" 'tabbar-backward) ; 前のタブ
+;; タブ上でマウスホイールを使わない
+(tabbar-mwheel-mode nil)
+;; グループを使わない
+(setq tabbar-buffer-groups-function nil)
+;; 左側のボタンを消す
+(dolist (btn '(tabbar-buffer-home-button
+               tabbar-scroll-left-button
+               tabbar-scroll-right-button))
+  (set btn (cons (cons "" nil)
+                 (cons "" nil))))
+;; タブ同士の間隔
+(setq tabbar-separator '(0.8))
+;; 外観変更
+(set-face-attribute			;バー自体の色
+ 'tabbar-default nil
+ :family (face-attribute 'default :family)
+ :background (face-attribute 'mode-line-inactive :background)
+ :height 0.9)
+(set-face-attribute			;アクティブなタブ
+ 'tabbar-unselected nil
+ :background (face-attribute 'mode-line-inactive :background)
+ :foreground (face-attribute 'mode-line-inactive :foreground)
+ :box nil)
+(set-face-attribute			;非アクティブなタブ
+ 'tabbar-selected nil
+ :background (face-attribute 'mode-line :background)
+ :foreground (face-attribute 'mode-line :foreground)
+ :box nil)
