d9a14313b31baa0afa05e3c8da86d86a6886d464
kakakaya <kakakaya+git@gmail.com>
2015-03-23 14:38:50 +0900

    init.el: added undohist config

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -492,6 +492,33 @@
   (require 'random-splash-image)
   (setq random-splash-image-dir (concat (getenv "HOME") "/.emacs.d/splash-images")) ; ランダムな画像のパス
   (random-splash-image-set))      ; ランダムな設定を実行
+
+;; undohist
+(el-get-bundle! undohist
+  (undohist-initialize)
+  (setq undohist-ignored-files '("/tmp/")) ; 永続化を無視するファイル名の正規表現
+  ;; NTEmacsだと動かないらしいので再定義
+  ;; http://d.hatena.ne.jp/Lian/20120420/1334856445
+  (when (eq system-type 'windows-nt)
+    (defun make-undohist-file-name (file)
+      (setq file (convert-standard-filename (expand-file-name file)))
+      (if (eq (aref file 1) ?:)
+          (setq file (concat "/"
+                             "drive_"
+                             (char-to-string (downcase (aref file 0)))
+                             (if (eq (aref file 2) ?/)
+                                 ""
+                               (if (eq (aref file 2) ?\\)
+                                   ""
+                                 "/"))
+                             (substring file 2))))
+      (setq file (expand-file-name
+                  (subst-char-in-string
+                   ?/ ?!
+                   (subst-char-in-string
+                    ?\\ ?!
+                    (replace-regexp-in-string "!" "!!"  file)))
+                  undohist-directory)))))
 ;; ========================================
 ;;             require 'package
 ;; ========================================
