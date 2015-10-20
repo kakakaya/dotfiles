f74fa7da7ed3ea63cdea30638672883389527fe7
kakakaya <kakakaya+git@gmail.com>
2015-10-20 17:18:58 +0900

    changed file name

--- home/bin/DebianInstall.sh
+++ home/bin/DebianInstall.sh
@@ -32,7 +32,7 @@ LC_ALL=C xdg-user-dirs-update --force
 sudo ln -s /usr/bin/nodejs /usr/local/bin/node
 
 #making dir
-mkdir -p ~/.fonts ~/tmp ~/mnt ~/git
+mkdir -p ~/.fonts ~/tmp ~/mnt ~/git ~/.ssh
 
 chsh -s /bin/zsh kakakaya
 
@@ -40,13 +40,6 @@ chsh -s /bin/zsh kakakaya
 dropbox start -i
 
 exit 0
-
-# ln -s Dropbox/Backups/config/.skk-jisyo .skk-jisyo
-# ln -s Dropbox/howm howm         # For later
-
-## ssh
-mkdir .ssh
-# ssh-keygen -t rsa
 
 # urxvt config
 # mkdir -p ~/.urxvt/
@@ -57,50 +50,4 @@ mkdir .ssh
 # chmod u+x clipboard
 # cd
 
-## Emacs
-# mkdir -p ~/.emacs.d/elisp
-# wget http://www.emacswiki.org/emacs/download/auto-install.el ~/.emacs.d/elisp
-# emacs --batch -Q -f batch-byte-compile ~/.emacs.d/elisp/auto-install.el
-
-# git clone https://github.com/emacs-helm/helm ~/.emacs.d/
-# cd ~/.emacs.d/helm && make && cd
-
-
-## Emacs memo
-# cd ~/tmp && git clone https://github.com/zk-phi/sublimity && cd
-# wget 'https://sites.google.com/site/rtfreesoft/font-drop/mona-izmg16.7z' # mona font
-
 # get catimg.sh
-
-## must package-install:
-# main-line
-# markdown-mode
-# highlight-symbol
-# auto-highlight-symbol
-# undo-tree
-# flex-autopair
-# anzu
-# f
-# hlinum
-# volatile-highlights
-# lua-mode
-# js2-mode
-# coffee-mode
-# twittering-mode
-# elpy
-# emmet-mode
-
-## emacswiki:
-
-# Unknown
-# (require 'dired-x )		;diredを便利にする
-# (require 'wdired )			;diredから"r"でファイル名をインライン編集
-# (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
-# (require 'wgrep-helm )		;M-x grepする、*grep*バッファでC-c C-pすると書き換わる
-# (setq dired-listing-switches "-AFGhlrt")
-# (require 'flyspell nil t) ;スペルチェック、要設定重点
-
-# (require 'markdown-mode nil t)
-# (setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
-# (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
--- home/bin/DropboxInstall.sh
+++ home/bin/DropboxInstall.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+# -*- mode:shell-script; coding:utf-8 -*-
+# Author: kakakaya, Date: Tue Oct 20 17:08:36 2015
 
 if ![ -d $HOME/Dropbox/bin ]; then
+    echo "Run this script after Dropbox's sync end!"
     exit 1;
 fi
 
 # Make symbolic link
 ln -s $HOME/Dropbox/howm $HOME/howm         # For later
-ln -s $HOME/Dropbox/ $HOME/
-ln -s $HOME/ $HOME/
-ln -s $HOME/ $HOME/
+ln -s $HOME/Dropbox/Workspace $HOME/Workspace
+ln -s $HOME/Dropbox/Study $HOME/Study
+ln -s $HOME/Dropbox/Work $HOME/Work
+# ln -s $HOME/Dropbox/ $HOME/
 
 # Install fonts
 if [ -d $HOME/.fonts ] ; then
@@ -20,4 +24,3 @@ fi
 # Install jlisting.sty
 sudo cp ~/Dropbox/config/jlisting.sty /usr/share/texlive/texmf-dist/tex/latex/listings/
 sudo mktexlsr
-
