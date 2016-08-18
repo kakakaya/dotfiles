76a1a11bb191417d7ebc21caef987491a9d69cb9
kakakaya <kakakaya+git@gmail.com>
2016-08-18 20:49:26 +0900

    Divided .zshrc, use zplug

---
+++ home/.zsh/50-zplug.zsh
@@ -0,0 +1,21 @@
+# -*- Mode: shell-script;coding:utf-8 -*- #
+
+# fzf
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
+
+# enhancd
+zplug "b4b4r07/enhancd", use:init.sh
+
+# emoji-cli provides input completion for emoji on the command line with an interactive filter.
+zplug "b4b4r07/emoji-cli"
+export EMOJI_CLI_KEYBIND="^[e"
+zplug "mrowa44/emojify", as:command, use:emojify rename-to:emojify
+
+
+zplug "mollifier/cd-gitroot"
+
+# compinit 以降に読み込むようにロードの優先度を変更する（10~19にすれば良い）
+# 色が醜い
+# zplug "zsh-users/zsh-syntax-highlighting", nice:10
+
+zplug load
