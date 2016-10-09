04979ec80736fdfb600422e144acf2152e1760ff
kakakaya <kakakaya+git@gmail.com>
2016-10-09 20:58:35 +0900

    Fixing bin/debian-*

--- home/bin/spacemacs-install.sh
+++ home/bin/spacemacs-install.sh
@@ -2,23 +2,26 @@
 # -*- mode:shell-script; coding:utf-8 -*-
 # Author: kakakaya, Date: Wed Apr 27 11:36:04 2016
 
-emacs_dir=$(file $HOME/.emacs.d | cut -d ' ' -f 2)
-if [ $emacs_dir == 'cannot' ]; then
+emacs_dir=$(file "$HOME/.emacs.d" | cut -d ' ' -f 2)
+if [ "$emacs_dir" == 'cannot' ]; then
     # $HOME/.emacs.d: cannot open `$HOME/.emacs.d' (No such file or directory)
     git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
     return 0
 else
     while true; do
-        read -p "$HOME/.emacs.d is "$emacs_dir". Move and install Spacemacs?[yN]" yn
+        read -p "$HOME/.emacs.d is $emacs_dir. Move and install Spacemacs?[yN]" yn
         case $yn in
             [Yy]* )
                 echo "install."
-                mv $HOME/.emacs.d $HOME/.emacs.d.$(date +%Y-%m-%d_%H-%M-%S)
+                mv "$HOME/.emacs.d" "$HOME/.emacs.d.$(date +%Y-%m-%d_%H-%M-%S).bak"
                 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-                exit 0;;
+                return 0
+                ;;
+
             * )
                 echo "exit."
-                exit 1;;
+                return 1
+                ;;
         esac
     done
 fi
