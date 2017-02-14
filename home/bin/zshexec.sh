a7716cf3c4d02554ec080a626d6283464b85b339
kakakaya <kakakaya+git@gmail.com>
2017-02-14 13:40:27 +0900

    added fortune for zshexec

--- home/bin/zshexec.sh
+++ home/bin/zshexec.sh
@@ -4,12 +4,17 @@
 # PROCCOUNT=`expr \`ps -Afl | wc -l\` - 5`
 if [ "$SHLVL" -le 2 ]; then
     [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
-    if [[ -x /usr/bin/figlet ]]; then
+    if hash fortune >& /dev/null; then
         if [ -e /usr/share/figlet/colossal.flf ]; then
             figlet -f colossal $host 2>/dev/null
         else
             figlet $host 2>/dev/null
         fi
     fi
+    if hash fortune >& /dev/null; then
+        # show some fortune
+        fortune -a
+    fi
     w
+    if 
 fi
